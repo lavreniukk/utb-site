473afe7d2c5181b2e274baaadd9e044f9eb8e4b6
Andrii Lavreniuk <lavreniuk.andrey@gmail.com>
2023-03-03 16:27:37 +0200

    started doing layout and routing

---
+++ src/routing/router.jsx
@@ -0,0 +1,4 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes.jsx";
+
+export const App = () => <RouterProvider router={router}/>;
---
+++ src/routing/routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/MainPage/MainPageComponent";
+import { Layout } from "./layout";
+import Contact from "../components/ContactComponent/Contact";
+
+export const router = createBrowserRouter ([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                element: <Home/>,
+                index: true,
+            },
+            {
+                path: "/contact",
+                element: <Contact/>,
+            },
+        ],
+    },
+]);
