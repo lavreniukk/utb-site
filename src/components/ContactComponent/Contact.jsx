ecc332fd05cb55680fff3bde14475acc83cd52c6
Andrii Lavreniuk <lavreniuk.andrey@gmail.com>
2023-07-28 17:31:51 +0300

    contact page added

--- src/components/ContactComponent/Contact.jsx
+++ src/components/ContactComponent/Contact.jsx
@@ -1,6 +1,54 @@
+import { 
+    Container, 
+    Row, 
+    Col, 
+    Card,
+    CardBody,
+    CardTitle,
+    CardSubtitle
+} from "reactstrap";
+import './contactstyles.css';
+import { Link } from "react-router-dom";
+
 export default function Contact(props) {
     document.title = 'УТБ Ресурс - ' + props.title; 
     return (
-        <div> Contact </div>
+        <div>
+            <Container className="mb-5">
+                <h1 className="text-center text-uppercase mt-5 mb-5">Контакти</h1>
+                <Row xs="1" md="2">
+                    <Col className="d-flex justify-content-center">
+                        <Card className='mb-3 styled-contact-card'>
+                            <CardBody>
+                                <div className="contact-icon d-flex justify-content-center mb-2">
+                                    <i className="fa-solid fa-envelope"></i>
+                                </div>
+                                <CardTitle tag='h4' className="text-center mb-4">
+                                    Електронна пошта
+                                </CardTitle>
+                                <CardSubtitle tag='h5' className="mb-3">
+                                    <a href="mailto: email.example@gmail.com" className="contact-link">email.example@gmail.com</a>
+                                </CardSubtitle>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <Card className='mb-3 styled-contact-card'>
+                            <CardBody>
+                                <div className="contact-icon d-flex justify-content-center mb-2">
+                                    <i className="fa-solid fa-phone"></i>
+                                </div>
+                                <CardTitle tag='h4' className="text-center mb-4">
+                                    Номер телефону
+                                </CardTitle>
+                                <CardSubtitle tag='h5' className="mb-3">
+                                    <a href="tel:+380667065896" className="contact-link">+38 (066) 123 45 67</a>
+                                </CardSubtitle>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
--- src/components/UIComponents/SliderComponent/Slider.jsx
+++ src/components/UIComponents/SliderComponent/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from "swiper";
 import "swiper/css";
@@ -16,6 +16,17 @@ function Slider({items}) {
         navigation={true}
         modules={[Pagination, Navigation]}
         className="mySwiper p-5 mb-5"
+        breakpoints={{
+          576: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          992: {
+            slidesPerView: 3,
+          },
+        }}
       >
         {items.map((item) => <SwiperSlide><img src={item} alt='alt'/></SwiperSlide>)}
         {items.map((item) => <SwiperSlide><img src={item} alt='alt'/></SwiperSlide>)}
