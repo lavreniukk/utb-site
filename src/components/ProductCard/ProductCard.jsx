d5ba1529b40ab607573a0f337ba7f03a7e25e717
Andrii Lavreniuk <lavreniuk.andrey@gmail.com>
2023-09-11 20:21:30 +0300

    changed article style in product card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -11,7 +11,8 @@ function ProductCard({product, image}) {
           <Image src={image} title={product.title}/>
         </div>
         <CardBody className='product-card__body'>
-            <CardTitle tag='h4' className='product-card__title'>{product.name} {product.article}</CardTitle>
+            <CardTitle tag='h4' className='product-card__title'>{product.name}</CardTitle>
+            <CardTitle tag='h5' className='product-card__article'>{product.article}</CardTitle>
             <Link to={`/product/${product.id}`} className='product-card__btn'>Детальніше</Link>
         </CardBody>
     </Card>
