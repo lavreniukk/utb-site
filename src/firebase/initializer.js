55b151e657e275ec8e42c6c8823790af280b8fb5
lavreniukk <lavreniuk.andrey@gmail.com>
2023-09-19 17:40:00 +0300

    added initializer to git

---
+++ src/firebase/initializer.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+import { getStorage } from 'firebase/storage';
+import firebaseConfig from "./firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage();
+
+export { db, storage };
